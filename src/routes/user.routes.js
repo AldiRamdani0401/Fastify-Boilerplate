fd2d768fab6a599e3f9a7beded924e2a06693bf7
AldiRamdani0401 <ramdanialdi889@gmail.com>
2025-03-16 21:47:30 +0700

    fix: improve routes modules

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -2,9 +2,9 @@ import UserController from "../controllers/user.controller.js";
 import RoleMiddleware from "../middlewares/role.middleware.js";
 
 async function UserRoutes(fastify, options) {
-  fastify.addHook("preHandler", async (request) => {
-    RoleMiddleware("admin", request);
-  });
+  // fastify.addHook("preHandler", async (request) => {
+  //   RoleMiddleware("admin", request);
+  // });
 
   fastify.get("/users", UserController.getAllUsers);
   fastify.get("/users/:user", UserController.getUser);
